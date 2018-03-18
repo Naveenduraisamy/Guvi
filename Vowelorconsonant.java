b26cb136386a553479b9c6bb541c88158a9e044f
Naveenduraisamy <naveendurai183@gmail.com>
2018-03-18 16:48:53 +0530

    Update Vowelorconsonant.java

--- Vowelorconsonant.java
+++ Vowelorconsonant.java
@@ -3,10 +3,10 @@ class Vowel
 {
 public static void main(String args[])
 {
-char a[5]={'a','e','i','o','u'};
+char a[]={'a','e','i','o','u'};
 Scanner s=new Scanner(System.in);
-char b=s.nextchar();
-for(int i=0;i<5;i++)
+char b=s.next().charAt(0);
+for(int i=0;i<a.length;i++)
 {
 if(b==a[i])
 System.out.println("Vowel");
