6ae774d4d55fe8dbc58b10140dab0da34e006f82
Naveenduraisamy <naveendurai183@gmail.com>
2018-04-28 15:02:03 +0530

    Update stringreverse.java

--- stringreverse.java
+++ stringreverse.java
@@ -1,2 +1,9 @@
 import java.io.*;
 class stringreverse{
+  public static void main(String args[])
+  {
+    DataInputStream d=new DataInputStream(System.in);
+    String a=Integer.parseInt(d.readLine());
+    System.out.println(a.reverse());
+  }
+}
