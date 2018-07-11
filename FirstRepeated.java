91f2bae153a0661ef3ea7b687dd6e7b8b7eca639
Naveenduraisamy <naveendurai183@gmail.com>
2018-07-11 18:51:52 +0530

    Create FirstRepeated.java

---
+++ FirstRepeated.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class FirstRepeated
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int c=0;
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		for(int i=0;i<a;i++)
+		{
+			al.add(s.nextInt());
+		}
+		Set<Integer> st=new LinkedHashSet<Integer>(al);
+		for(int b:st)
+		{
+			c=Collections.frequency(al,b);
+			if(c!=1)
+			{
+			 System.out.print(b+" ");
+			 break;
+			}
+		}
+}
+}
