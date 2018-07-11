3dedbf6cdebae133643aa114498f8762044d72c8
Naveenduraisamy <naveendurai183@gmail.com>
2018-07-11 17:35:04 +0530

    Create Repeat.java

---
+++ Repeat.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Repeat
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int c=0;
+		ArrayList<Integer> al=new ArrayList<Integer>();
+		for(int i=0;i<a;i++)
+		{
+			al.add(s.nextInt());
+		}
+		Set<Integer> st=new TreeSet<Integer>(al);
+		for(int b:st)
+		{
+			c=Collections.frequency(al,b);
+			if(c!=1)
+			 System.out.print(b+" ");
+		}
+	}
+}
