5a7051491d41c41f2e38e47789c0f120ae244174
Naveenduraisamy <naveendurai183@gmail.com>
2018-07-12 18:10:28 +0530

    Update FirstRepeated.java

--- FirstRepeated.java
+++ FirstRepeated.java
@@ -1,11 +1,11 @@
 import java.util.*;
 class FirstRepeated
 {
-	public static void main (String[] args) throws java.lang.Exception
+	public static void main (String[] args)
 	{
 		Scanner s=new Scanner(System.in);
 		int a=s.nextInt();
-		int c=0;
+		int c=0,d=0;
 		ArrayList<Integer> al=new ArrayList<Integer>();
 		for(int i=0;i<a;i++)
 		{
@@ -18,8 +18,11 @@ class FirstRepeated
 			if(c!=1)
 			{
 			 System.out.print(b+" ");
+			 d++;
 			 break;
 			}
 		}
-}
+		if(d==0)
+		 System.out.println("unique");
+	}
 }
